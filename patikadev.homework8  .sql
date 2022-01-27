6c13dbc784705ee53d7883a44f7c1a592d3ec4d4
oguzhantemizer <95439861+oguzhantemizer@users.noreply.github.com>
2022-01-27 21:24:55 +0300

    Add files via upload

---
+++ patikadev.homework8  .sql
@@ -0,0 +1,11 @@
+SELECT*FROM employee
+--UPDATE employee SET name = 'Bony' WHERE name = 'Sarah'
+--UPDATE employee SET birthday='1996-11-18' WHERE id =2;
+--UPDATE employee SET name ='XXX' WHERE name LIKE '%y'
+--UPDATE employee SET name = 'Lottely' WHERE id = 8
+--UPDATE employee SET name ='Dore' WHERE name LIKE '%s' 
+--DELETE FROM employee WHERE id = 3
+--DELETE FROM employee WHERE name LIKE '%k'
+--DELETE FROM employee WHERE birthday = '1966-01-21'
+--DELETE FROM employee WHERE name = 'Jeni'
+--DELETE FROM employee  WHERE id = 23
