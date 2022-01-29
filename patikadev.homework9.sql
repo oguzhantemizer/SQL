f01183051f91534f32eaf318fd2a3238e872d1dc
Oğuzhan Temizer <95439861+oguzhantemizer@users.noreply.github.com>
2022-01-29 16:04:05 +0300

    Add files via upload

---
+++ patikadev.homework9.sql
@@ -0,0 +1,9 @@
+--Aşağıdaki sorgu senaryolarını dvdrental örnek veri tabanı üzerinden gerçekleştiriniz.
+-- city tablosu ile country tablosunda bulunan şehir (city) ve ülke (country) isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
+SELECT city.city, country.country FROM city INNER JOIN country on country.country_id=city.country_id;
+
+--customer tablosu ile payment tablosunda bulunan payment_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
+SELECT customer.first_name, customer.last_name FROM customer INNER JOIN payment ON payment.customer_id=customer.customer_id
+
+--customer tablosu ile rental tablosunda bulunan rental_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
+SELECT customer.first_name, customer.last_name,rental.rental_id FROM customer INNER JOIN rental ON rental.customer_id=customer.customer_id
